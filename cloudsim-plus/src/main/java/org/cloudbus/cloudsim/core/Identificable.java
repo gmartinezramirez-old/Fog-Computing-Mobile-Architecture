8c89e6054a09fd0ad57254c0d1509b995c8580d0
Gonzalo Martinez <gonzalo.martinez.ra@gmail.com>
2016-12-29 23:47:47 -0300

    Cleanup of deprecated code

--- cloudsim-plus/src/main/java/org/cloudbus/cloudsim/core/Identificable.java
+++
@@ -1,9 +0,0 @@
-package org.cloudbus.cloudsim.core;
-
-/**
- * An interface for objects that have to be identified by an id.
- * @author Manoel Campos da Silva Filho
- */
-public interface Identificable {
-    int getId();
-}
